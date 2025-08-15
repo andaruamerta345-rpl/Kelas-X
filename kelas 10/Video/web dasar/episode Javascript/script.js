10d6e9b2af1711a962fd0ce91f7d981cab1358f3
7arzz <andaruamerta345@gmail.com>
2025-08-15 15:34:41 +0700

    Add kelas 10 folder contents

---
+++ kelas 10/Video/web dasar/episode Javascript/script.js
@@ -0,0 +1,11 @@
+// alert("javascript");
+
+// function tampil() {
+//   document.getElementById("aku").innerHTML = "javascript";
+// }
+
+function tampil() {
+  document.querySelector("h1").innerHTML = "Belajar Javascript";
+  document.querySelector(".container").style.backgroundColor = "red";
+  document.querySelector("#para").style.fontSize = "50px";
+}
