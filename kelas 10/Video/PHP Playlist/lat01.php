550a3fc9930975f47135cc5a0fb251224650c2d8
7arzz <andaruamerta345@gmail.com>
2025-08-18 18:23:49 +0700

    PHP Menampilkan tulisan dan html

---
+++ kelas 10/Video/PHP Playlist/lat01.php
@@ -0,0 +1,21 @@
+<?php 
+
+    echo "Belajar PHP";
+
+    echo '<br>';
+
+    echo 'Saya siswa SMK';
+
+    echo '<h1 style="background-color:red">Belajar PHP itu mudah</h1>
+          <p style="background-color:blue">Saya suka belajar program</p>
+    ';
+
+    echo 'Saya belajar '.'PHP'.'<br>';
+
+    // echo 2019;
+    echo 2019 + 2;
+
+    // echo 'Tahun 2019';
+
+?>
+
