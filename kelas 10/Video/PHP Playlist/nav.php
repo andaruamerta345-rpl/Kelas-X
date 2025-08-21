493bd9dfa74d37516e87268c8fc9bb3c4b9913ed
7arzz <andaruamerta345@gmail.com>
2025-08-21 17:16:55 +0700

    PHP Require_once

---
+++ kelas 10/Video/PHP Playlist/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <h1>Navbar</h1>
+
+    <ul>
+        <li>Kontak</li>
+        <li>Sejarah</li>
+        <li>Jurusan</li>
+    </ul>
+</nav>
---
+++ kelas 10/Video/PHP Playlist/require.php
@@ -0,0 +1,16 @@
+<?php 
+
+// require_once adalah perintah untuk menyertakan/menampilkan file PHP lain
+// Jika file tersebut sudah disertakan sebelumnya, maka tidak akan disertakan lagi.
+
+    require_once 'nav.php';
+
+    echo "<hr>";
+
+    require_once 'lat04.php';
+    require_once 'lat04.php';
+    // require_once akan menghasilkan error fatal jika file tidak ditemukan
+
+
+
+?>
