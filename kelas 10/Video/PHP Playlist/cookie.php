ec3150bcedc4701e6df39fbef008040856ce4748
andaruamerta345-rpl <andaruamerta345@gmail.com>
2025-08-23 07:34:57 +0700

    PHP upload file

--- kelas 10/Video/PHP Playlist/cookie.php
+++ kelas 10/Video/PHP Playlist/cookie.php
@@ -10,12 +10,7 @@
     $cookie_value = 'Tejo';
     setcookie($cookie_name, $cookie_value);
 
-    // Tampilkan cookie (hanya muncul setelah refresh)
-    if (isset($_COOKIE[$cookie_name])) {
-        echo $_COOKIE[$cookie_name];
-    } else {
-        echo "Cookie belum tersedia, silakan refresh halaman.";
-    }
+
 
     // Hapus cookie
     setcookie("user", "", time() - 3600);
