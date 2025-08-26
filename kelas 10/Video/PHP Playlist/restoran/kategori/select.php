ebef0c77fafcb7897660c1ea74541f74859eec3c
7arzz <7arzz404@gmail.com>
2025-08-26 15:50:04 +0700

    PHP Select data

---
+++ kelas 10/Video/PHP Playlist/restoran/kategori/select.php
@@ -0,0 +1,41 @@
+<?php 
+
+
+    require_once "../function.php";
+
+    $sql = "SELECT * FROM tblkategori";
+
+    // echo $sql
+
+    $result  = mysqli_query($koneksi , $sql);
+
+    // var_dump($result);
+
+    $jumlah = mysqli_num_rows($result);
+    // echo "<br>";
+    echo "<h3>" . "Jumlah baris ada : " . $jumlah ."</h3>" . "<br>";
+
+
+    echo '
+    <table border="1px">
+    <tr>
+        <th>No</th>
+        <th>Kategori</th>
+    </tr>
+    ';
+$no=1;
+    if ($jumlah > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+            echo '<tr>';
+            // echo '<td>' . $row['idkategori'] . '</td>';
+            echo '<td>' . $no++ . '</td>';
+            echo '<td>' . $row['kategori'] . '</td>';
+            echo '</tr>';
+        }
+
+    }
+
+    echo '</table>'
+
+
+?>
