96f1b765aefead357e99ca478743a2063e7a5211
7arzz <7arzz404@gmail.com>
2025-08-27 19:19:42 +0700

    PHP Paging

--- kelas 10/Video/PHP Playlist/restoran/kategori/select.php
+++ kelas 10/Video/PHP Playlist/restoran/kategori/select.php
@@ -1,19 +1,47 @@
+<div style="margin:auto; width: 900px;">
 <?php 
 
 
     require_once "../function.php";
+    
 
-    $sql = "SELECT * FROM tblkategori";
 
-    // echo $sql
+    // Query pertama: hitung jumlah data
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result  = mysqli_query($koneksi , $sql);
+    $jumlahdata = mysqli_num_rows($result);
+    // echo "<h3>" . "Jumlah data ada : " . $jumlahdata ."</h3>" . "<br>";
+    
+    
+    
+    // Query kedua: ambil data dengan limit
+    // $mulai = 3;
+    $banyak = 3;
+    $halaman = ceil($jumlahdata / $banyak);
+    // echo "<h3>" . "Jumlah halaman ada : " . $halaman ."</h3>" . "<br>";
 
+    for ($i=1; $i <= $halaman ; $i++) { 
+        echo '<a href="?p='.$i.'">'.$i.'</a>'.'&nbsp &nbsp &nbsp';
+    };
+    echo '<br> <br>';
+
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+        // echo "Halaman saat ini adalah : " . $p;
+        $mulai = ($p * $banyak) - $banyak;
+        //   3 =  (2  *  3    ) - 3
+    }else {
+         $mulai = 0;
+    }
+
+    $sql = "SELECT * FROM tblkategori Limit $mulai, $banyak";
     $result  = mysqli_query($koneksi , $sql);
 
+    // echo $sql
     // var_dump($result);
-
     $jumlah = mysqli_num_rows($result);
     // echo "<br>";
-    echo "<h3>" . "Jumlah baris ada : " . $jumlah ."</h3>" . "<br>";
+    // echo "<h3>" . "Jumlah baris ada : " . $jumlah ."</h3>" . "<br>";
 
 
     echo '
@@ -23,7 +51,7 @@
         <th>Kategori</th>
     </tr>
     ';
-$no=1;
+$no=$mulai+1;
     if ($jumlah > 0){
         while ($row = mysqli_fetch_assoc($result)){
             echo '<tr>';
@@ -35,7 +63,10 @@ $no=1;
 
     }
 
-    echo '</table>'
+    echo '</table>';
+    
+    
+    ?>
 
-
-?>
+<!-- <h1>belajar</h1> -->
+</div>
