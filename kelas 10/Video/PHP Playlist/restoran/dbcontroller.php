b396c9137a30f5083b63a94ea2c228b0ee40f340
7arzz <7arzz404@gmail.com>
2025-08-30 20:44:26 +0700

    PHP Playlist : Project aplikasi restoran -> Class database

---
+++ kelas 10/Video/PHP Playlist/restoran/dbcontroller.php
@@ -0,0 +1,85 @@
+<?php 
+
+    class DB {
+
+        private $host = "127.0.0.1";
+        private $user = "root";
+        private $password = "";
+        private $database = "dbrestoran";
+        private $koneksi;
+
+        public function __construct()
+        {
+            $this->koneksi = $this->koneksiDB();
+        }
+
+        public function koneksiDB()
+        {
+        $koneksi = mysqli_connect(
+            $this->host,
+            $this->user,
+            $this->password,
+            $this->database
+        );
+        return $koneksi;
+        }
+
+        public function getALL($sql) {
+            $result = mysqli_query($this -> koneksi, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                $data[]=$row;
+            }
+            
+            return $data;
+        }
+        
+        public function getITEM($sql)
+        {
+            $result = mysqli_query($this -> koneksi, $sql);
+            $row = mysqli_fetch_assoc($result);
+            return $row;
+        }
+        
+        public function rowCOUNT($sql)
+        {
+            $result = mysqli_query($this -> koneksi, $sql);
+            $count = mysqli_num_rows($result);
+            
+            return $count;
+        }
+        
+        public function runSQL($sql)
+        {
+            $result = mysqli_query($this -> koneksi, $sql);
+        }
+
+        public function pesan($text = "")
+        {
+            echo $text;
+        }
+
+    }
+
+    // $db = new DB;
+    // $db -> pesan("berhasil");
+    // $db -> runSQL("INSERT INTO tblkategori VALUES ('','Minuman')");
+    // $db -> runSQL("DELETE FROM tblkategori WHERE idkategori = 23");
+    // var_dump($db)
+
+/*
+    // $row = $db -> getALL("SELECT * FROM tblkategori");
+    $row = $db -> getITEM("SELECT * FROM tblkategori WHERE idkategori = 12");
+    $count = $db -> rowCOUNT("SELECT * FROM tblkategori");
+    // var_dump($row);
+    echo "Menu yang anda pilih adalah : " . $row['kategori'] . '<br/>';
+    echo "Total menu yang ada disini adalah : " . $count;
+*/
+
+/*
+    foreach ($row as $key) {
+        echo $key['kategori'] . '<br/>';
+    }
+*/
+
+
+?>
