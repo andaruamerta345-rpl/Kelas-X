ab3547d0cdccf30d78ce302a9c6c5d4d3a121e33
7arzz <7arzz404@gmail.com>
2025-09-04 14:27:58 +0700

    PHP Playlist : Project aplikasi restoran -> Insert Order Detail

--- kelas 10/Video/PHP Playlist/restoran/home/checkout.php
+++ kelas 10/Video/PHP Playlist/restoran/home/checkout.php
@@ -2,9 +2,21 @@
 
     if (isset($_GET['total'])) {
         $total = $_GET['total'];
-        echo "Total pembelian anda adalah : ".$total . '<br/>';
-        echo idorder() . '<br/>';
-        echo insertOrder(idorder(),$_SESSION['idpelanggan'],'2025-09-04',$total);
+        $idorder = idorder();
+        $idpelanggan = $_SESSION['idpelanggan'];
+        $tgl = date('Y-m-d');
+        $sql = "SELECT * FROM tblorder WHERE idorder = $idorder";
+
+        $count = $db -> rowCOUNT($sql);
+        if ($count == 0) {
+            insertOrder($idorder,$idpelanggan,$tgl,$total);
+            insertOrderDetail($idorder);
+        }else {
+            insertOrderDetail($idorder);
+        }
+
+        // echo "Total pembelian anda adalah : ".$total . '<br/>';
+        // echo idorder() . '<br/>';
     }
 
     function idorder() {
@@ -16,7 +28,7 @@
             $id = 1;
         }else {
             $item = $db -> getITEM($sql);
-            $id = $item['idorder'];
+            $id = $item['idorder']+1;
         }
 
         return $id;
@@ -26,8 +38,31 @@
         global $db;
 
         $sql = "INSERT INTO tblorder VALUES ($idorder,$idpelanggan,'$tgl',$total,0,0,0)";
-        echo $sql;
+        // echo $sql;
         $db -> runSQL($sql);
+    }
+
+    function insertOrderDetail($idorder=1) {
+        global $db;
+
+        foreach ($_SESSION as $key => $value) {
+            if ($key<>'pelanggan' && $key<>'idpelanggan') {
+                $id = substr($key,1);
+                $sql = "SELECT * FROM tblmenu WHERE idmenu=$id";
+                $row = $db -> getALL($sql);
+                foreach ($row as $r) {
+                    $idmenu = $r['idmenu'];
+                    $harga = $r['harga'];
+                    $sql = "INSERT INTO tblorderdetail VALUES ('', $idorder, $idmenu, $value,$harga)";
+                    $db->runSQL($sql);
+                    // echo $sql;
+                }
+
+                // echo '<pre>';
+                // print_r($row);
+                // echo '</pre>';
+            }
+        }
     }
 
 ?>
