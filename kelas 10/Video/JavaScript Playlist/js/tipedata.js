5c69d1348906e4165901b658c56a5f02b5bcf709
7arzz <7arzz404@gmail.com>
2025-09-13 16:48:27 +0700

    JavaScript Playlist -> eps 3 | Tipe data

---
+++ kelas 10/Video/JavaScript Playlist/js/tipedata.js
@@ -0,0 +1,28 @@
+let angka = 10;
+let kata = "Saya Belajar JavaScript";
+let pecahan = 2.05;
+
+console.log(typeof angka);
+console.log(typeof kata);
+console.log(typeof pecahan);
+
+console.log(1 / 0);
+
+console.log("Javascript" / 2 + 5);
+
+let isi = "Belajar";
+console.log(isi + kata);
+
+console.log(` 'SMK Revit' ${angka} ${kata} ${isi}`);
+
+console.log("SMK Revit " + angka + " " + kata + " " + isi);
+
+// let kondisi = true;
+let kondisi = false;
+console.log(kondisi);
+
+let age = null;
+console.log(age);
+
+let kosong;
+console.log(kosong);
