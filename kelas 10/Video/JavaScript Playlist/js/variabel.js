9b7e9a9ae5dbf955aac14e275426abbbb218c622
7arzz <7arzz404@gmail.com>
2025-09-13 16:26:50 +0700

    JavaScript Playlist -> eps 1 & 2 | Penulisan koding / Variabel & Konstanta

---
+++ kelas 10/Video/JavaScript Playlist/js/variabel.js
@@ -0,0 +1,18 @@
+// let angka = 10;
+let angka;
+let kata = "Hello World";
+const data = 100;
+// data = 200;
+angka = 5;
+
+console.log(angka);
+console.log(kata);
+console.log(data);
+
+angka = 8;
+console.log(angka);
+
+// let isi = "Belajar";
+// let isi = "4";
+let isi = "4.6";
+console.log(typeof isi);
