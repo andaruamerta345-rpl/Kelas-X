eacab03acf08e10f6edfc046ca13999ac24ad7cd
7arzz <7arzz404@gmail.com>
2025-09-16 16:38:46 +0700

    JavaScript Playlist -> eps 23 | Shopping Cart #2

--- kelas-10/Video/JavaScript Playlist/js/shoppingcart.js
+++ kelas-10/Video/JavaScript Playlist/js/shoppingcart.js
@@ -9,8 +9,8 @@ let tblmenu = [
   {
     idmenu: 2,
     idkategori: 1,
-    menu: "Anggur",
-    gambar: "anggur.jpg",
+    menu: "ikan",
+    gambar: "ikan.jpeg",
     harga: 20000,
   },
   {
@@ -34,20 +34,42 @@ let tblmenu = [
     gambar: "risol.jpg",
     harga: 20000,
   },
+  {
+    idmenu: 6,
+    idkategori: 3,
+    menu: "Putu Ayu",
+    gambar: "putuayu.jpg",
+    harga: 20000,
+  },
 ];
 
 let tampil = tblmenu.map(function (kolom) {
   return `
-    <div class="card m-2" style="width: 12rem;">
-      <img src="assets/${kolom.gambar}" class="card-img-top" alt="${kolom.menu}">
-      <div class="card-body">
-        <h5 class="card-title">${kolom.menu}</h5>
-        <p class="card-text">Rp. ${kolom.harga}</p>
-        <a href="#" class="btn btn-primary">Buy</a>
-      </div>
+    <div class="col-md-4 d-flex justify-content-center">
+        <div class="card m-2" style="width: 12rem;">
+            <img src="assets/${kolom.gambar}" class="card-img-top" alt="${kolom.menu}">
+            <div class="card-body">
+                <h5 class="card-title">${kolom.menu}</h5>
+                <p class="card-text">Rp. ${kolom.harga}</p>
+                <div class="btn-beli">
+                    <button class="btn btn-primary" data-idmenu="${kolom.idmenu}">Beli</button>
+                </div>
+            </div>
+        </div>
     </div>
   `;
 });
-let isi = document.querySelector(".produk");
+let isi = document.querySelector(".row");
 isi.innerHTML = tampil.join("");
 console.log(tampil);
+
+let btnbeli = document.querySelectorAll(".btn-beli button");
+let cart = [];
+
+for (let i = 0; i < btnbeli.length; i++) {
+  btnbeli[i].onclick = function () {
+    // console.log(btnbeli[i].dataset["idmenu"]);
+    cart.push(btnbeli[i].dataset["idmenu"]);
+  };
+}
+console.log(cart);
