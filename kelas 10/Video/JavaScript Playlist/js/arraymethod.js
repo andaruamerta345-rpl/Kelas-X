07e45558cfbddd16bb2c39a02c37590249955e2d
7arzz <7arzz404@gmail.com>
2025-09-16 12:41:28 +0700

    JavaScript Playlist -> eps 20 | Array Methods #2 | Concat, ForEach, Filter

--- kelas 10/Video/JavaScript Playlist/js/arraymethod.js
+++ kelas 10/Video/JavaScript Playlist/js/arraymethod.js
@@ -1,3 +1,4 @@
+// ========== Array Method #1 ==========
 // arrray -> sting, number, boolean, object, function, array, campuran
 
 let nilai = [
@@ -7,11 +8,39 @@ let nilai = [
   { nama: "siti", ipa: 90, bahasa: 80, matematika: 90 },
 ];
 let nama = ["budi", "joni", "tejo", "siti"];
-nama.push("ani", "roma");
-nama.unshift("bobi", "roki");
+// nama.push("ani", "roma");
+// nama.unshift("bobi", "roki");
 // console.log(nama.splice(5, 2));
-console.log(nama.slice(0, 3));
+// console.log(nama.slice(0, 3));
 // console.log(nilai.shift());
 // console.log(nama.pop());
 // console.log(nilai)[0].nama;
-console.log(nama[joni]);
+// console.log(nama[joni]);
+
+// ========== Array Method #2 ==========
+let mapel = ["ipa", "bahasa", "matematika"];
+// console.log(nama.concat(mapel));
+// console.log(nama.concat(["ips", "pkn", "sejarah"]));
+
+// for (let index = 0; index < nama.length; index++) {
+//   console.log(nama[index]);
+// }
+
+// nama.forEach(function (a) {
+//   console.log(a);
+// });
+
+// nama.forEach((a) => console.log(a));
+
+// nilai.filter(function (a) {
+//   if (a.ipa > 80) {
+//     console.log(a.nama);
+//   }
+// });
+
+// console.log(nilai);
+
+// nilai.filter((a) => (a.ipa > 80 ? console.log(a) : null));
+nilai.filter((a) =>
+  a.ipa > 80 && a.matematika > 80 ? console.log(a.nama) : null
+);
