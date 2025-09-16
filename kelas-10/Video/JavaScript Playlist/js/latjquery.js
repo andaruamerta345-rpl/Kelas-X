bb10f24c40ca8e08e050fb00e361e5aebff6c40f
7arzz <7arzz404@gmail.com>
2025-09-16 20:00:02 +0700

    JavaScript Playlist -> eps 32 | Jquery | Event

--- kelas-10/Video/JavaScript Playlist/js/latjquery.js
+++ kelas-10/Video/JavaScript Playlist/js/latjquery.js
@@ -5,4 +5,14 @@ $(document).ready(function () {
 $(function () {
   //   console.log("Document ready");
   $("#isi").html("<h1>Hello jQuery</h1>");
+
+  $("#klik").click(function (e) {
+    e.preventDefault();
+    alert("Belajar jQuery");
+  });
+
+  $("#isi").mouseleave(function () {
+    alert("Mouse keluar area");
+    console.log("Mouse keluar area");
+  });
 });
