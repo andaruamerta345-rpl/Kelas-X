609f1cb48922e83ce63fae9d7886e539fb892a5b
7arzz <7arzz404@gmail.com>
2025-09-17 15:29:29 +0700

    JavaScript Playlist -> eps 34 | Ajax Jquery PHP #2 - Layout

--- kelas-10/Video/AJAX-JQUERY/index.php
+++ kelas-10/Video/AJAX-JQUERY/index.php
@@ -10,7 +10,63 @@
      <script src="js/jquery.js"></script>
     </head>
     <body>
-        <h1>Belajar Ajax-Jquery Bootstrap PHP</h1>
+        <div class="container">
+            <div class="row mt-4">
+                <h1>Belajar Ajax-Jquery Bootstrap PHP</h1>
+            </div>
+            <div class="row mt-4">
+                <div class="col-sm">
+                    <div class="row">
+                        <h2>Input Data Pelanggan</h2>
+                    </div>
+                    <div class="row">
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Pelanggan :</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <div id="emailHelp" class="form-text">Harus diisi!</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Alamat :</label>
+                                <input type="text" class="form-control" id="alamat">
+                                <div id="emailHelp" class="form-text">Harus diisi!</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Telp :</label>
+                                <input type="tel" class="form-control" id="telp">
+                                <div id="emailHelp" class="form-text">Harus diisi!</div>
+                            </div>
+                            <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-sm">
+                  <div class="row">
+                    <h2>Data Pelanggan</h2>
+                  </div>
+                  <div class="row">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Pelanggan</th>
+                                <th scope="col">Alamat</th>
+                                <th scope="col">Telp</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                  </div>
+                </div>
+            </div>
+        </div>
         
    <!-- <script>
     $(document).ready(function(){
