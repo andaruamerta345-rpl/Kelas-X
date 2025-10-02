bffa7ac7bf64db739c66f46ac59616442fc08cb7
7arzz <7arzz404@gmail.com>
2025-10-02 18:37:02 +0700

    2-10-25

--- kelas-10/Kelas/23-9-25/main.cpp
+++ kelas-10/Kelas/23-9-25/main.cpp
@@ -7,42 +7,37 @@ int main()
     cout << "Masukkan Nilai Anda : ";
     cin >> nilai;
     // nilai = 0;
-
-    if (nilai < 0 || nilai > 100) {
-        cout << "Emang ada nilai segitu?? yang bener ajaa bangg" << endl;
-    } else if (nilai ==  100) {
-        cout << "Selamat nilai anda SEMPURNA : "<< nilai << "Kelas bang, pertahanin yaa" << endl;
-    } else if (nilai >= 90) {
-        cout << "Predikat anda A dengan nilai : " << nilai << endl;
-    }else if (nilai >= 80) {
-        cout << "Predikat anda B dengan nilai : " << nilai << endl;
-    }else if (nilai >= 70) {
-        cout << "Predikat anda C dengan nilai : " << nilai << endl;
-    }else if (nilai >= 60) {
-        cout << "Predikat anda D dengan nilai : " << nilai << endl;
-    }else if (nilai == 0){
-        cout << "Bang?? Sumpah belajar lagi bang, nilai lu " << nilai << " lhoo, parah banget "<< endl;
-    }else {
-        cout << "Predikat anda E dengan nilai : " << nilai << endl;
-    }
-
-    // if (nilai < 0 || nilai > 100) {
-    //     cout << "Emang ada nilai segitu?? yang bener ajaa bangg" << endl;
-    // } if (nilai ==  100) {
-    //     cout << "Selamat nilai anda SEMPURNA : "<< nilai << "Kelas bang, pertahanin yaa" << endl;
-    // } if (nilai >= 90) {
+    // if (nilai >= 90) {
     //     cout << "Predikat anda A dengan nilai : " << nilai << endl;
-    // } if (nilai >= 80) {
+    // }else if (nilai >= 80) {
     //     cout << "Predikat anda B dengan nilai : " << nilai << endl;
-    // } if (nilai >= 70) {
+    // }else if (nilai >= 70) {
     //     cout << "Predikat anda C dengan nilai : " << nilai << endl;
-    // } if (nilai >= 60) {
+    // }else if (nilai >= 60) {
     //     cout << "Predikat anda D dengan nilai : " << nilai << endl;
-    // } if (nilai == 0){
+    // }else if (nilai == 0){
     //     cout << "Bang?? Sumpah belajar lagi bang, nilai lu " << nilai << " lhoo, parah banget "<< endl;
     // }else {
     //     cout << "Predikat anda E dengan nilai : " << nilai << endl;
     // }
+
+    if (nilai < 0 && nilai > 100) {
+        cout << "Emang ada nilai segitu?? yang bener ajaa bangg" << endl;
+    } if (nilai ==  100) {
+        cout << "Selamat nilai anda SEMPURNA : "<< nilai << "Kelas bang, pertahanin yaa" << endl;
+    } if (nilai >= 90) {
+        cout << "Predikat anda A dengan nilai : " << nilai << endl;
+    } if (nilai >= 80) {
+        cout << "Predikat anda B dengan nilai : " << nilai << endl;
+    } if (nilai >= 70) {
+        cout << "Predikat anda C dengan nilai : " << nilai << endl;
+    } if (nilai >= 60) {
+        cout << "Predikat anda D dengan nilai : " << nilai << endl;
+    } if (nilai == 0){
+        cout << "Bang?? Sumpah belajar lagi bang, nilai lu " << nilai << " lhoo, parah banget "<< endl;
+    }else {
+        cout << "Predikat anda E dengan nilai : " << nilai << endl;
+    }
 
     return 0;
 }
