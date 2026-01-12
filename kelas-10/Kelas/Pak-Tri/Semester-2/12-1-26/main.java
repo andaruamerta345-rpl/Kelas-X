a51f4691b2188fedcf54d8c6ffd7a9db5116cd2a
7arzz <7arzz404@gmail.com>
2026-01-12 09:28:19 +0700

    12-1-26

---
+++ kelas-10/Kelas/Pak-Tri/Semester-2/12-1-26/main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        // ====== type data int ======
+        int angka;
+        angka = 10;
+        System.out.println(angka);
+        System.out.println("angka yang anda input adalah " + angka);
+
+
+        angka = 100;
+        System.out.println("angka yang anda input terbaru adalah " + angka);
+
+
+        // ====== type data double ======
+        double angka2 = 10.5;
+        System.out.println(angka2);
+
+        angka2 = 100.5;
+        System.out.println("angka yang anda input terbaru adalah " + angka2);
+
+        // ====== type data char  ======
+        char karakter = 'A';
+        System.out.println(karakter);
+        karakter = 'B';
+        System.out.println("karakter yang anda input adalah " + karakter);
+
+        // ====== type data boolean ======
+        boolean benar = true;
+        System.out.println(benar);
+        benar = false;
+        System.out.println("nilai boolean yang anda input adalah " + benar);
+
+        // ===== type data String ======
+        String nama = "Andaru";
+        System.out.println(nama);
+        nama = "Dayat";
+        System.out.println("nama yang anda input adalah " + nama);
+
+        // ====== type data final ======
+        final String NAMA_SEKOLAH = "SMKN 2 Buduran Sidoarjo";
+        System.out.println(NAMA_SEKOLAH);
+    }
+}
