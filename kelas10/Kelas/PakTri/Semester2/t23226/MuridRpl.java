c83992b75ce62ad9d3483cf0ccd4793f12e14e06
7arzz <7arzz404@gmail.com>
2026-02-23 09:07:19 +0700

    23-2-26

---
+++ kelas10/Kelas/PakTri/Semester2/t23226/MuridRpl.java
@@ -0,0 +1,18 @@
+package kelas10.Kelas.PakTri.Semester2.t23226;
+public class MuridRpl {
+    String nama = "Asep";
+    int absen = 1;
+    int usia = 17; 
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getAbsen() {
+        return absen;
+    }
+
+    public int getUsia() {
+        return usia;
+    }
+}
---
+++ kelas10/Kelas/PakTri/Semester2/t23226/SepuluhRpl.java
@@ -0,0 +1,10 @@
+package kelas10.Kelas.PakTri.Semester2.t23226;
+public class SepuluhRpl {
+    public static void main(String[] args) {
+      MuridRpl objek = new MuridRpl();
+        System.out.println("Nama   : " + objek.getNama());
+        System.out.println("Absen  : " + objek.getAbsen());
+        System.out.println("Usia   : " + objek.getUsia());
+    }    
+}
+
