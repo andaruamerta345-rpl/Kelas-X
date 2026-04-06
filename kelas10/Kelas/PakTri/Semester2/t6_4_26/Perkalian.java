af5b3b851f99c8f0a433a422fd97d089ebd24afc
7arzz <7arzz404@gmail.com>
2026-04-06 08:14:32 +0700

    6 4 26

---
+++ kelas10/Kelas/PakTri/Semester2/t6_4_26/Perkalian.java
@@ -0,0 +1,23 @@
+public class Perkalian {
+
+    // Method 1: perkalian 2 bilangan int
+    static int perkalian(int a, int b) {
+        return a * b;
+    }
+
+    // Method 2: perkalian 3 bilangan int
+    static int perkalian(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    // Method 3: perkalian 2 bilangan double
+    static double perkalian(double a, double b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Perkalian 2 angka bulat: " + perkalian(2, 3));
+        System.out.println("Perkalian 3 angka bulat: " + perkalian(2, 3, 4));
+        System.out.println("Perkalian 2 angka desimal: " + perkalian(2.5, 3.5));
+    }
+}
