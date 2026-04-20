3b55e501772670f9852cb74ef101b58edca257e5
7arzz <7arzz404@gmail.com>
2026-04-20 09:23:49 +0700

    20 april 2026

---
+++ kelas10/Kelas/PakTri/Semester2/t20_4_26/Contoh.java
@@ -0,0 +1,24 @@
+public class Contoh {
+    public static void main(String[] args) {
+        Objek obj1 = new Objek("Budi", "X RPL", 15);
+        Objek obj2 = new Objek("Ani", "XI RPL", 16);
+        Objek obj3 = new Objek("Citra", "XII RPL", 17);
+        Objek obj4 = new Objek("Dedi", "X RPL", 15); 
+        Objek obj5 = new Objek("Eka", "XI RPL", 16);
+        Objek obj6 = new Objek("Fahmi", "XII RPL", 17);
+        Objek obj7 = new Objek("Gita", "X RPL", 15);
+        Objek obj8 = new Objek("Hani", "XI RPL", 16);
+        Objek obj9 = new Objek("Indah", "XII RPL", 17);
+        Objek obj10 = new Objek("Joko", "X RPL", 15);
+        Objek obj11 = new Objek("Kiki", "XI RPL", 16);
+        Objek obj12 = new Objek("Lina", "XII RPL", 17);
+        Objek obj13 = new Objek("Maman", "X RPL", 15);
+        Objek obj14 = new Objek("Nani", "XI RPL", 16);
+        Objek obj15 = new Objek("Oka", "XII RPL", 17);
+        Objek obj16 = new Objek("Panca", "X RPL", 15);
+        Objek obj17 = new Objek("Qori", "XI RPL", 16);
+        Objek obj18 = new Objek("Rina", "XII RPL", 17);
+        Objek obj19 = new Objek("Siti", "X RPL", 15);
+        Objek obj20 = new Objek("Tono", "XI RPL", 16);
+    }
+}
---
+++ kelas10/Kelas/PakTri/Semester2/t20_4_26/Objek.java
@@ -0,0 +1,15 @@
+public class Objek {
+    String nama;
+    String kelas;
+    int umur;
+
+    public Objek(String nama, String kelas, int umur) {
+        this.nama = nama;
+        this.kelas = kelas;
+        this.umur = umur;
+        
+        System.out.println("Nama saya " + nama + " Umur saya " + umur + " Kelas saya " + kelas);
+    }
+}
+
+
